785e5a6a61e0374cd19124e8914b29c1fffcec91
Garima-msft <v-garisi@microsoft.com>
2021-08-12 17:50:42 +0530

    Update Project.ext.ps1

--- oss-config-staging/1.10.6/Project.ext.ps1
+++ oss-config-staging/1.10.6/Project.ext.ps1
@@ -1,12 +1,15 @@
-Set-StrictMode -Version Latest 
-class ProjectExt: Project {    
-    ProjectExt([string] $subscriptionId, [SVTResource] $svtResource): Base($subscriptionId, $svtResource) {
-    }
+Set-StrictMode -Version Latest
+  class ProjectExt: Project
+  {
+  	ProjectExt([string] $subscriptionId, [SVTResource] $svtResource): Base($subscriptionId, $svtResource)
+  {
 
-    hidden [ControlResult] CheckPublicProjects([ControlResult] $controlResult)
-	{
-                $controlResult.AddMessage([VerificationResult]::verify,"Test message.");
+  }
+  	hidden [ControlResult] CheckPrivateProjects([ControlResult] $controlResult)
+  	{
+          $controlResult.AddMessage([VerificationResult]::Failed,
+                                              "From github repo");
 
-    }
-   
-}
+      return $controlResult;
+  	}
+  }
